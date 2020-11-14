610ee3f262314e0c3fc7af06a45480757c8f8ca7
Fawful <64529004+riaaaaaaaa@users.noreply.github.com>
2020-11-14 15:37:01 +1300

    Update radraveraid.py

--- radraveraid.py
+++ radraveraid.py
@@ -1,9 +1,9 @@
 # coding=utf8
 class INIT:
-    __version__ = 1.7
+    __version__ = 1.8
 
-import json,requests,os,time,random,ctypes,selenium
-from threading import Thread
+import json,requests,os,time,random,ctypes,selenium,websocket,sys
+from concurrent.futures import ThreadPoolExecutor
 from colorama import Fore
 req = requests.Session()
 
@@ -15,38 +15,42 @@ tokens = open('tokens.txt','r').read().splitlines()
 proxies = open('proxies.txt','r').read().splitlines()
 proxies = [{'https':'http://'+proxy} for proxy in proxies]
 
+executor = ThreadPoolExecutor(max_workers=config["threadcap"])
+
 def Setup():
-    ctypes.windll.kernel32.SetConsoleTitleW(f'[RadRaveRaid v{INIT.__version__}] | By ria')
+    ctypes.windll.kernel32.SetConsoleTitleW(f'[RadRaveRaid v{INIT.__version__}] | By Fawful')
     counttokens = len(open('tokens.txt').readlines())
     countproxies = len(open('proxies.txt').readlines())
 
-    print(f'''{Fore.RESET}
-           {Fore.MAGENTA}    ____            __{Fore.RESET}{Fore.CYAN}   ____                 {Fore.RESET}{Fore.RED}     ____        _     __{Fore.RESET}
-           {Fore.MAGENTA}   / __ \____ _____/ /{Fore.RESET}{Fore.CYAN}  / __ \____ __   _____ {Fore.RESET}{Fore.RED}    / __ \____ _(_)___/ /{Fore.RESET}
-           {Fore.MAGENTA}  / /_/ / __ `/ __  / {Fore.RESET}{Fore.CYAN} / /_/ / __ `/ | / / _ \{Fore.RESET}{Fore.RED}   / /_/ / __ `/ / __  / {Fore.RESET}
-           {Fore.MAGENTA} / _, _/ /_/ / /_/ /  {Fore.RESET}{Fore.CYAN}/ _, _/ /_/ /| |/ /  __/{Fore.RESET}{Fore.RED}  / _, _/ /_/ / / /_/ /  {Fore.RESET}
-           {Fore.MAGENTA}/_/ |_|\__,_/\__,_/  {Fore.RESET}{Fore.CYAN}/_/ |_|\__,_/ |___/\___/ {Fore.RESET}{Fore.RED} /_/ |_|\__,_/_/\__,_/   {Fore.RESET}
+    print(f'''{Fore.RESET}                {Fore.MAGENTA}    ____            __{Fore.RESET}{Fore.CYAN}   ____                 {Fore.RESET}{Fore.RED}     ____        _     __{Fore.RESET}
+                {Fore.MAGENTA}   / __ \____ _____/ /{Fore.RESET}{Fore.CYAN}  / __ \____ __   _____ {Fore.RESET}{Fore.RED}    / __ \____ _(_)___/ /{Fore.RESET}
+                {Fore.MAGENTA}  / /_/ / __ `/ __  / {Fore.RESET}{Fore.CYAN} / /_/ / __ `/ | / / _ \{Fore.RESET}{Fore.RED}   / /_/ / __ `/ / __  / {Fore.RESET}
+                {Fore.MAGENTA} / _, _/ /_/ / /_/ /  {Fore.RESET}{Fore.CYAN}/ _, _/ /_/ /| |/ /  __/{Fore.RESET}{Fore.RED}  / _, _/ /_/ / / /_/ /  {Fore.RESET}
+                {Fore.MAGENTA}/_/ |_|\__,_/\__,_/  {Fore.RESET}{Fore.CYAN}/_/ |_|\__,_/ |___/\___/ {Fore.RESET}{Fore.RED} /_/ |_|\__,_/_/\__,_/   {Fore.RESET}
 
 
-                   {Fore.CYAN}RadRaveRaid {INIT.__version__} {Fore.RESET}| Because fuck you
-                   {Fore.CYAN}Type {Fore.RESET}"help"{Fore.CYAN} for a list of commands
-                   {Fore.GREEN}{counttokens}{Fore.RESET}{Fore.CYAN} tokens loaded!
-                   {Fore.GREEN}{countproxies}{Fore.RESET}{Fore.CYAN} proxies loaded!
-                   {Fore.MAGENTA}Created by {Fore.RESET}Fawful |{Fore.CYAN} Thx to {Fore.RESET}Humble#8292{Fore.CYAN} for several fixes
-                   {Fore.MAGENTA}https://github.com/{Fore.RESET}riaaaaaaaa
+                       {Fore.CYAN}RadRaveRaid {INIT.__version__} {Fore.RESET}| Because fuck you
+                       {Fore.CYAN}Type {Fore.RESET}"help"{Fore.CYAN} for a list of commands
+                       {Fore.GREEN}{counttokens}{Fore.RESET}{Fore.CYAN} tokens loaded!
+                       {Fore.GREEN}{countproxies}{Fore.RESET}{Fore.CYAN} proxies loaded!
+                       {Fore.MAGENTA}Created by {Fore.RESET}Fawful | {Fore.CYAN}Special thx to {Fore.RESET}Humble#8292{Fore.CYAN}, {Fore.RED}RaidToolBox{Fore.CYAN}, and {Fore.YELLOW}h0nda
+                       {Fore.MAGENTA}https://github.com/{Fore.RESET}riaaaaaaaa
 ''' + Fore.RESET)
 
 def Help():
-    print(f'''{Fore.RESET}{Fore.RED}                   -- Commands are seperated by commas --{Fore.RESET}
+    print(f'''{Fore.RESET}{Fore.RED}                       -- Commands are seperated by commas --{Fore.RESET}
         
 {Fore.CYAN}join >> (invite) | {Fore.RESET}Joins all your tokens to the server using the specified invite.
 {Fore.CYAN}leave >> (serverid) | {Fore.RESET}Joins all your tokens to the server using the specified invite.
+{Fore.CYAN}bringonline | {Fore.RESET}Brings all of your tokens online {Fore.RED}*
 {Fore.CYAN}spam  >> (channelid) (amount) (message) | {Fore.RESET}Spams the given channel any amount of times. Tokens must already be in server.
-{Fore.CYAN}friend  >> (username#discrimanator) | {Fore.RESET}Makes all your tokens send a friend request to your target.
-{Fore.CYAN}dm >> (userid) (amount) (message) | {Fore.RESET}Makes all your tokens DM the given ID.
+{Fore.CYAN}friend  >> (username#discrimanator) | {Fore.RESET}Makes all your tokens send a friend request to your target. {Fore.RED}*
+{Fore.CYAN}dm >> (userid) (amount) (message) | {Fore.RESET}Makes all your tokens DM the given ID. {Fore.RED}*
 {Fore.CYAN}check-tokens | {Fore.RESET}Checks all of your tokens to see if they are valid.
 {Fore.CYAN}scrape-proxies | {Fore.RESET}Scrapes HTTP proxies from proxyscrape.com and writes them to proxies.txt.
 {Fore.CYAN}reset | {Fore.RESET}Resets the console.
+
+{Fore.RED}* LIKELY TO LOCK UNVERIFIED TOKENS, USE WITH CAUTION
 ''' + Fore.RESET)
     Start()
 
@@ -184,6 +188,51 @@ def Check():
         print(f"{Fore.YELLOW}[ERROR]: {Fore.YELLOW}{e}"+Fore.RESET)
     Start()
 
+def StausChange(tok):
+    ws = websocket.WebSocket()
+    ws.connect('wss://gateway.discord.gg/?v=6&encoding=json')
+    hello = json.loads(ws.recv())
+    heartbeat_interval = hello['d']['heartbeat_interval']
+    auth = {
+        "op": 2,
+        "d": {
+            "token": tok,
+            "properties": {
+                "$os": sys.platform,
+                "$browser": "RTB",
+                "$device": f"{sys.platform} Device"
+            },
+            "presence": {
+                "status": "online",
+                "since": 0,
+                "afk": False
+            }
+        },
+        "s": None,
+        "t": None
+    }
+    ws.send(json.dumps(auth))
+    ack = {
+        "op": 1,
+        "d": None
+    }
+    while True:
+        time.sleep(heartbeat_interval/1000)
+        try:
+            ws.send(json.dumps(ack))
+        except Exception:
+            break
+
+def BringOnline():
+    try:
+        print(f"[{Fore.GREEN}+{Fore.RESET}] Bringing online...")
+        for tok in tokens:
+            executor.submit(StausChange,tok)
+        print(f"[{Fore.GREEN}+{Fore.RESET}] Finished!")
+    except Exception as e:
+        print(f"{Fore.YELLOW}[ERROR]: {Fore.YELLOW}{e}"+Fore.RESET)
+    Start()
+
 def Clear():
     os.system('cls')
 
@@ -215,6 +264,8 @@ def Start():
     elif command[0] == 'join':
         invite = command[1]
         Join(invite)
+    elif command[0] == 'bringonline':
+        BringOnline()
     elif command[0] == 'leave':
         serverid = command[1]
         Leave(serverid)
